d8d26f3229e58ba916780de76c0ef08eddfc1311
Mriyam Tamuli <mbtamuli@gmail.com>
2021-07-13 08:52:40 +0530

    :ok_hand: IMPROVE: Fix linting issues
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- minimal-py-app/app.py
+++ minimal-py-app/app.py
@@ -1,13 +1,22 @@
+"""
+Minimal application for demo.
+"""
 from flask import Flask, make_response
 
 app = Flask(__name__)
 
 @app.route("/errorz")
 def errorz():
-	return "Something broke!", 500
+    """
+    Handle the /errorz route, return 500 HTTP Status Code.
+    """
+    return "Something broke!", 500
 
 @app.route("/healthz")
 def healthz():
-    r = make_response('{"status":"ok"}')
-    r.mimetype = 'application/json'
-    return r
+    """
+    Handle the /healthz route, return 200 HTTP Status Code and a JSON response
+    """
+    response = make_response('{"status":"ok"}')
+    response.mimetype = 'application/json'
+    return response
